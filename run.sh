a3bc860bb08a27ccc6f4e60d1c3d687ca44d7ba4
PrimeHXR <81677579+PrimeHXR@users.noreply.github.com>
2025-04-14 10:35:45 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,19 +10,16 @@ tmpfs /home/o11/hls tmpfs defaults,noatime,nosuid,nodev,noexec,mode=1777,size=70
 tmpfs /home/o11/dl tmpfs defaults,noatime,nosuid,nodev,noexec,mode=1777,size=70% 0 0
 EOL
 
-# Mount all entries in /etc/fstab
 mount -av
 
-# Infinite loop to check if o11 is running
 while true; do
   if ! pgrep "o11v4" > /dev/null; then
-    # Start the o11 process
+
     /home/o11/o11v4 -p 8484 -noramfs -f /usr/bin/ffmpeg -path "/home/o11/" -noautostart -plstreamname "%s [%p]" &
     
-    # Wait before checking again to give the process time to start
+
     sleep 10
   fi
   
-  # Wait before checking again
   sleep 20
 done
